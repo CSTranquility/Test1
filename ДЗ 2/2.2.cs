0be3bedda1a0e2b2f586f6145b7ecfcc6254ade0
CSTranquility <40829770+CSTranquility@users.noreply.github.com>
2018-08-13 15:42:19 +0300

    ДЗ 2

---
+++ ДЗ 2/2.2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        public static void Main()
+        {
+            Human h = new Human(100, 20);
+            Random r = new Random();
+            while(true)
+            {
+                h.TakeDamage(r.Next(10, 100));
+                Thread.Sleep(100);
+            }
+        }
+    }
+    class Warrior
+    {
+        protected int _health;
+        protected Warrior(int health)
+        {
+            _health = health;
+        }
+        protected void DeadMessage()
+        {
+            if (_health < 0)
+                Console.WriteLine("Я умер");
+        }
+    }
+    class Wombat:Warrior
+    {
+        private int _armor;
+
+        public Wombat(int health,int armor):base(health)
+        {
+            _armor = armor;
+        }
+        public void TakeDamage(int damage)
+        {
+            _health -= damage - _armor;
+            base.DeadMessage();
+        }
+    }
+
+    class Human:Warrior
+    {
+        private int _agility;
+        public Human(int health, int agility) : base(health)
+        {
+            _agility = agility;
+        }
+        public void TakeDamage(int damage)
+        {
+            _health -= damage / _agility;
+            DeadMessage();
+        }
+    }
+}
+
+
+
---
+++ ДЗ 2/2.5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Vector
+    {
+        public int X;
+        public int Y;
+        public Vector(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector (v1.X + v2.X, v1.Y + v2.Y);
+        }
+        public static Vector operator *(Vector v, int multiplier)
+        {
+            return new Vector(v.X*multiplier,v.Y*multiplier);
+        }
+    }
+}
+
+
+
